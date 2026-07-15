45799b3838b3befcec63c3152c4d929a09b4b93e
AndreyAstahov <andrey.astahov@astrazeneca.com>
2026-07-15 18:55:06 +0700

    Initial Commit

---
+++ Newz/NewzApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewzApp.swift
+//  Newz
+//
+//  Created by Astakhov Andrey on 15.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct NewzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
